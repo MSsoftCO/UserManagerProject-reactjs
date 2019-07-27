53a0fba9a1c7623568ddb099b45c58691d5c3b36
Morteza <mssoftco@gmail.com>
2019-07-27 11:19:13 +0430

    Updates

---
+++ precache-manifest.deb043de84b8e7ee77da620ad3e10604.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922a8dd18127809ce7a5f8e91572dbec",
+    "url": "/UserManagerProject-reactjs/index.html"
+  },
+  {
+    "revision": "f85658aecdde6ecb96b0",
+    "url": "/UserManagerProject-reactjs/static/css/2.39cab565.chunk.css"
+  },
+  {
+    "revision": "7b08b13e94b3102e2033",
+    "url": "/UserManagerProject-reactjs/static/css/main.0c1a600d.chunk.css"
+  },
+  {
+    "revision": "f85658aecdde6ecb96b0",
+    "url": "/UserManagerProject-reactjs/static/js/2.7a511b50.chunk.js"
+  },
+  {
+    "revision": "7b08b13e94b3102e2033",
+    "url": "/UserManagerProject-reactjs/static/js/main.d5549cec.chunk.js"
+  },
+  {
+    "revision": "8f7bab424ab483116871",
+    "url": "/UserManagerProject-reactjs/static/js/runtime~main.61b2ed70.js"
+  },
+  {
+    "revision": "0ae68fe9a34451462b4a917bbb84e5ef",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Black.0ae68fe9.eot"
+  },
+  {
+    "revision": "163edc9f0e77cdd5f2d8fd60b3b83406",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Black.163edc9f.woff2"
+  },
+  {
+    "revision": "5667ccbb024f56b7501fbefe80909c3c",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Black.5667ccbb.woff"
+  },
+  {
+    "revision": "fa89dbf0e6b7e107f7209d36aa4a5190",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Black.fa89dbf0.ttf"
+  },
+  {
+    "revision": "4774ad8ec818ee81fc470ecdc3e8831f",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Bold.4774ad8e.eot"
+  },
+  {
+    "revision": "51bd611abab32dd3f1659d054b290b6f",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Bold.51bd611a.woff2"
+  },
+  {
+    "revision": "56dce27ac716170e603286ba0d440de0",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Bold.56dce27a.woff"
+  },
+  {
+    "revision": "9ec1b4dd9a4d4916ed17ecf952552e66",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Bold.9ec1b4dd.ttf"
+  },
+  {
+    "revision": "122071db7286d40324cb22c75ebac1f8",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Light.122071db.eot"
+  },
+  {
+    "revision": "d5bcad0cb424cc2a7cca1e9178fdecf9",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Light.d5bcad0c.woff2"
+  },
+  {
+    "revision": "ecdfbb9ddc7b46dba62f7ba73e84bfea",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Light.ecdfbb9d.ttf"
+  },
+  {
+    "revision": "eee1fa58b4f97ccfe668954c67571f2f",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Light.eee1fa58.woff"
+  },
+  {
+    "revision": "01e07ecc7ef15aac97ad543c46500b73",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Medium.01e07ecc.woff"
+  },
+  {
+    "revision": "14d12966397606370e52017e5312887b",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Medium.14d12966.woff2"
+  },
+  {
+    "revision": "9f8a7b6da82ed90759a1cd03477a0b91",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Medium.9f8a7b6d.eot"
+  },
+  {
+    "revision": "d9369334a85109c002f0093a8bbb5990",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Medium.d9369334.ttf"
+  },
+  {
+    "revision": "731561a6ca3e2b4aa76f152a4b534fb1",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Thin.731561a6.woff"
+  },
+  {
+    "revision": "7a0b4950d20ea06f783184e89f5aae2f",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Thin.7a0b4950.ttf"
+  },
+  {
+    "revision": "9c24f84ae131313dda7e723dffe280ad",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Thin.9c24f84a.woff2"
+  },
+  {
+    "revision": "cddbd4ba7244b014884f772823b7a93a",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir-Thin.cddbd4ba.eot"
+  },
+  {
+    "revision": "03ed11c59bca0b7f21f27cbb86a09c20",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir.03ed11c5.woff"
+  },
+  {
+    "revision": "7e32f522be5c69507074619fcaa7fff5",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir.7e32f522.eot"
+  },
+  {
+    "revision": "b295a26db726e74748c7280264b5d5d4",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir.b295a26d.ttf"
+  },
+  {
+    "revision": "c353f7ef8a50a4c7c282c2b30250738c",
+    "url": "/UserManagerProject-reactjs/static/media/Vazir.c353f7ef.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/UserManagerProject-reactjs/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/UserManagerProject-reactjs/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/UserManagerProject-reactjs/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/UserManagerProject-reactjs/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/UserManagerProject-reactjs/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
